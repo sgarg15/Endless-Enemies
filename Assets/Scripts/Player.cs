6a4135486161a33bc68dee1a5e207c0d153278f1
sgarg03 <sat.garg03@gmail.com>
2020-05-21 22:44:57 -0700

    added player and gun control

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerControl))]
+[RequireComponent (typeof (GunController))]
+public class Player : MonoBehaviour {
+
+  public float moveSpeed = 5;
+
+  Camera viewCamera;
+  PlayerControl controller;
+  GunController gunController;
+
+  void Start() {
+    controller = GetComponent<PlayerControl> ();
+    gunController = GetComponent<GunController> ();
+    viewCamera = Camera.main;
+  }
+
+  void Update() {
+    //Movement Input
+    Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+    controller.Move(moveVelocity);
+
+    //Look Input
+    Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
+    Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+    float rayDistance;
+
+    if(groundPlane.Raycast(ray, out rayDistance)){
+      Vector3 pointOfIntersection = ray.GetPoint(rayDistance);
+      //Debug.DrawLine(ray.origin, pointOfIntersection, Color.red);
+      controller.LookAt(pointOfIntersection);
+    }
+
+    //Weapon Input
+    if(Input.GetMouseButton(0)){
+      gunController.Shoot();
+    }
+  }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerControl : MonoBehaviour {
+
+  Vector3 velocity;
+  Rigidbody myRigidBody;
+  void Start() {
+    myRigidBody = GetComponent<Rigidbody> ();
+  }
+
+  public void Move(Vector3 _velocity){
+    velocity = _velocity;
+  }
+
+  public void LookAt(Vector3 lookPoint){
+    Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+    transform.LookAt(heightCorrectedPoint);
+  }
+
+  public void FixedUpdate(){
+    myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
+  }
+}
