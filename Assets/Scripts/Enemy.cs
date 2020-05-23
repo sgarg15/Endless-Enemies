18945f2747eadbb4c082ccc06964a1d2bf05caae
sgarg03 <sat.garg03@gmail.com>
2020-05-22 17:13:29 -0700

    added enemies and bullet ineraction

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class Enemy : MonoBehaviour {
+
+  NavMeshAgent pathFinder;
+  Transform target;
+
+  void Start() {
+    pathFinder = GetComponent<NavMeshAgent> ();
+    target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    StartCoroutine(UpdatePath());
+  }
+
+  void Update() {
+  }
+
+  IEnumerator UpdatePath(){
+    float refershRate = 0.25f;
+
+    while (target != null){
+      Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+      pathFinder.SetDestination (target.position);
+      yield return new WaitForSeconds(refershRate);
+    }
+  }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
+  public LayerMask collisionMask;
   float speed = 10;
 
   public void SetSpeed(float newSpeed){
     speed = newSpeed;
   }
 
-  // Start is called before the first frame update
-  void Start() {
-
-  }
-
   // Update is called once per frame
   void Update() {
-    transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    float moveDistance = speed * Time.deltaTime;
+    CheckCollisions(moveDistance);
+    transform.Translate(Vector3.forward * moveDistance);
+  }
+
+  void CheckCollisions(float moveDistance){
+    Ray ray = new Ray(transform.position, transform.forward);
+    RaycastHit hit;
+
+    if(Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide)){
+      OnHitObject(hit);
+    }
+  }
+
+  void OnHitObject(RaycastHit hit){
+    print(hit.collider.gameObject.name);
+    GameObject.Destroy (gameObject);
   }
 }
