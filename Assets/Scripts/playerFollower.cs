35830b64568318db7bf0bb6e7747dde8ee028e4b
sgarg03 <sat.garg03@gmail.com>
2020-05-31 16:44:05 -0700

    added script to follow player

--- Assets/Scripts/playerFollower.cs
+++ Assets/Scripts/playerFollower.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerFollower : MonoBehaviour {
+public class PlayerFollower : MonoBehaviour {
 
-  // Start is called before the first frame update
-  void Start() {
-      
+  public Transform player;
+
+  public float smoothSpeed = 10f;
+  public Vector3 offset;
+  bool playerIsDead;
+  LivingEntity _playerEntity;
+
+  void Start(){
+    _playerEntity = FindObjectOfType<Player> ();
+    _playerEntity.OnDeath += PlayerDeath;
   }
 
-  // Update is called once per frame
-  void Update() {
-      
+  void FixedUpdate() {
+    if(!playerIsDead){
+      Vector3 desiredPosition = player.position + offset;
+      Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+      transform.position = smoothedPosition;
+    }
+  }
+
+  void PlayerDeath(){
+    playerIsDead = true;
   }
 }
