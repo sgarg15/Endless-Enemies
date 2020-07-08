7c30e489ec42410c15d0479ca98d6e2a57556e52
sgarg03 <sat.garg03@gmail.com>
2020-07-08 16:43:08 -0700

    added console app

---
+++ The Shooter Server/The Shooter Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_Shooter_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
